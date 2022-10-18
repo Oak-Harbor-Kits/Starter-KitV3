8d971a25efc9f361a75dd5dba817307f8af52f9e
glaring gibbon <glaringgibbon@gmail.com>
2022-10-18 18:01:28 +0100

    Update 11ty config to pass through

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,11 @@
 
 module.exports = function(eleventyConfig) {
 
+  // Pass through assets icons TODO: Move icon files to icn folder and amend links.
+  eleventyConfig.addPassthroughCopy("./src/*.png");
+  eleventyConfig.addPassthroughCopy("./src/*.ico");
+  eleventyConfig.addPassthroughCopy("./src/*.xml");
+  eleventyConfig.addPassthroughCopy("./src/*.webmanifest");
   // Pass through assets CSS, fonts, images, JS :
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/fnt");
