4658d8bdcd24db299aad26be81f6a6e352f596b6
glaring gibbon <glaringgibbon@gmail.com>
2022-10-18 19:49:13 +0100

    Amended all links removing html, deleted nav.js

--- src/css/nav.js
+++
@@ -1,9 +0,0 @@
-
-// Toggle navigation open 
-
-const navbarMenu = document.querySelector("#navigation #navbar-menu");
-const hamburgerMenu = document.querySelector("#navigation .hamburger-menu");
-
-hamburgerMenu.addEventListener('click', function() {
-    navbarMenu.classList.toggle("open");
-});
